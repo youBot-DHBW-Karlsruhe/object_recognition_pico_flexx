4083883e991817029fb1630547b05c3004964cc9
thedomdom <dominik.wunderlich@gmx.de>
2018-03-15 08:38:22 +0100

    transform to state machine

--- scripts/learner.py
+++ scripts/learner.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
+import time
+
 import cv2
 import roslib
 import rospy
-from cv_bridge import CvBridge, CvBridgeError
+from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
-import time
 
 import helper
 
@@ -18,44 +19,52 @@ class ObjectLearner:
     def __init__(self):
         self.image_sub = rospy.Subscriber("/royale_camera_driver/depth_image", Image, self.callback)
         self.cv_bridge = CvBridge()
+
         self.timestamp_last_call = time.time()
+        self.state = "find_objects"
+        self.pressed_key = -1
 
     def callback(self, img_msg):
         # Prevent running multiple callbacks at once
         if time.time() > self.timestamp_last_call + 1:
             self.timestamp_last_call = time.time()
-            self.learn_object(img_msg)
+            self.learn_objects(img_msg)
         # else:
-            # print("Ignored")
+        # print("Ignored")
 
-    def learn_object(self, img_msg):
+    def learn_objects(self, img_msg):
 
-        try:
-            cv_image = self.cv_bridge.imgmsg_to_cv2(img_msg, "32FC1")
-            # Convert 32fc1 to 8uc1 (Gray scale)
-            image = (cv_image * 255).astype('u1')
-            # helper.show_image(image, "Origin")
+        if self.state == "find_objects":
+            self.find_objects(img_msg)
 
-            contours = helper.get_contours(image, True)
+        if self.state == "track_object":
+            rospy.loginfo("Tracking object...")
 
-            image_show = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
-            colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255), (255, 0, 255)]
-            for index, contour in enumerate(contours):
-                if index < 5:
-                    # drawContours(image, contours, contourIdx, color, thickness)
-                    cv2.drawContours(image_show, contours, index, colors[index], 1)
-            helper.show_image(image_show, "Learner")
+        if self.pressed_key == 27:  # 27 = Escape key
+            rospy.signal_shutdown("User Shutdown")
 
-            pressed_key = cv2.waitKey(500) & 255
-            # if pressed_key != -1:
-            #     print(pressed_key)
-            if pressed_key == ord('s'):
-                rospy.loginfo("Saving object...")
-            if pressed_key == 27:  # 27 = Escape key
-                rospy.signal_shutdown("User Shutdown")
+    def find_objects(self, img_msg):
+        cv_image = self.cv_bridge.imgmsg_to_cv2(img_msg, "32FC1")
+        # Convert 32fc1 to 8uc1 (Gray scale)
+        image = (cv_image * 255).astype('u1')
+        # helper.show_image(image, "Origin")
+        contours = helper.get_contours(image, True)
+        image_show = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+        colors = {"blue": (255, 0, 0), "green": (0, 255, 0), "red": (0, 0, 255), "yellow": (0, 255, 255)}
+        for index, contour in enumerate(contours):
+            if index < len(colors):
+                # drawContours(image, contours, contourIdx, color, thickness)
+                cv2.drawContours(image_show, contours, index, colors.values()[index], 1)
+        helper.show_image(image_show, "Learner")
 
-        except CvBridgeError as e:
-            print(e)
+        self.pressed_key = cv2.waitKey(500) & 255
+        for index, contour in enumerate(contours):
+            if index < len(colors):
+                if self.pressed_key == ord(colors.keys()[index][0]):
+                    rospy.loginfo("Tracking the " + colors.keys()[index] + " object. Do you want to save it? (y/n)")
+                    self.tracking_color = colors.values()[index]
+                    self.tracking_contour = contour
+                    self.state = "track_object"
 
 
 def main():
