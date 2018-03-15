97f972c2fcac6efa64e563d3f208b2cff7a5dcfe
thedomdom <dominik.wunderlich@gmx.de>
2018-03-15 16:13:54 +0100

    Code reformat

--- scripts/helper.py
+++ scripts/helper.py
@@ -118,6 +118,7 @@ def adjust_gamma(image, val_min=60, val_max=100):
     # apply gamma correction using the lookup table
     return cv2.LUT(image, table)
 
+
 def convert_img_msg(cv_bridge, img_msg):
     cv_image = cv_bridge.imgmsg_to_cv2(img_msg, "32FC1")
     # Convert 32fc1 to 8uc1 (Gray scale)
