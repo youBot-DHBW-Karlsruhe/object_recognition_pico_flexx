1937ba107752d97c867ae25a563396df59936075
thedomdom <dominik.wunderlich@gmx.de>
2018-04-20 13:32:17 +0200

    Organizing Imports

--- scripts/Recognizer.py
+++ scripts/Recognizer.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
-import time
-
 import cv2
 import roslib
 import rospy
-from cv_bridge import CvBridge
+
 from sensor_msgs.msg import Image
 
 from Detector import Detector
