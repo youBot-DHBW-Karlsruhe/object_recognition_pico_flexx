e0a6844c1f8dccff202ff0c0f64413e5d860dd2e
thedomdom <dominik.wunderlich@gmx.de>
2018-04-20 13:31:29 +0200

    Making Recognizer Object-Oriented

--- scripts/Recognizer.py
+++ scripts/Recognizer.py
@@ -9,62 +9,49 @@ import rospy
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 
-import helper
-from ObjectManager import ObjectManager
+from Detector import Detector
 
 roslib.load_manifest('object_recognition_pico_flexx')
 
 
 # http://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber%28python%29
-class Recognizer:
+class Recognizer(Detector):
 
     def __init__(self):
-        self.image_sub = rospy.Subscriber("/royale_camera_driver/depth_image", Image, self.callback)
-        self.cv_bridge = CvBridge()
+        Detector.__init__(self)
+        self.main_method = self.recognize_objects
 
-        self.timestamp_last_call = time.time()
-
-        object_manager = ObjectManager("../objects/objects.json")
-        self.objects = object_manager.load_objects()
+        self.objects = self.object_manager.load_objects()
 
         # self.pub = rospy.Publisher('/recognized_objects', String, queue_size=10)
         self.image_pub = rospy.Publisher("object_recognizer_visualization", Image, queue_size=10)
 
-    def callback(self, img_msg):
-        # Prevent running multiple callbacks at once
-        if time.time() > self.timestamp_last_call + 1:  # time.time() in seconds
-            self.timestamp_last_call = time.time()
-            self.recognize_objects(img_msg)
-
-    def recognize_objects(self, img_msg):
-
-        image_bw, image_rgb = helper.convert_img_msg(self.cv_bridge, img_msg)
-        contours = helper.get_contours(image_bw, False)
-
+    def recognize_objects(self):
         recognized_contours = {}
+
         for obj in self.objects:
-            index, difference = helper.find_best_matching_contour(obj["contour"], contours)
+            contour_index, difference = self.find_matching_contour(obj["contour"])
 
-            if index is not None:
-                if index not in recognized_contours or recognized_contours[index] > difference:
-                    recognized_contours[index] = (obj, difference)
+            if contour_index is not None:
+                if contour_index not in recognized_contours or recognized_contours[contour_index] > difference:
+                    recognized_contours[contour_index] = (obj, difference)
 
-        for index, (obj, difference) in recognized_contours.iteritems():
+        for contour_index, (obj, difference) in recognized_contours.iteritems():
             # rospy.loginfo("Found object with name '" + obj["name"] + "'!")
             # Show best result
-            # print("Contour Length:", len(contours[index]))
+            # print("Contour Length:", len(contours[contour_index]))
             print("Difference:", difference)
 
-            cv2.drawContours(image_rgb, contours, index, helper.colors["green"], 1)
-            angle = helper.get_contour_angle_on_image(contours[index], image_rgb, helper.colors["green"])
-            center = helper.get_center_on_image(contours[index], image_rgb, helper.colors["red"])
+            cv2.drawContours(self.image_rgb, self.contours, contour_index, self.colors["green"], 1)
+            angle = self.get_contour_angle_on_image(contour_index, self.colors["green"])
+            center = self.get_center_on_image(contour_index, self.colors["red"])
 
             font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(image_rgb, obj["name"], center, font, 0.5, helper.colors["pink"])
+            cv2.putText(self.image_rgb, obj["name"], center, font, 0.5, self.colors["pink"])
 
             # print("Angle in scene", angle)
 
-        helper.show_image(image_rgb, "Recognized Objects")
+        self.show_image(self.image_rgb, "Recognized Objects")
         pressed_key = cv2.waitKey(500) & 255
         if pressed_key == 27:  # 27 = Escape key
             rospy.signal_shutdown("User Shutdown")
